bf4a42aff7154711ad2390410a9559f72bd883fc
filip <filip@DESKTOP-FLQPSSC>
2019-04-10 18:11:13 +0200

    JavaDoc

--- MyRobots/src/group03/DaggeV2.java
+++ MyRobots/src/group03/DaggeV2.java
@@ -13,46 +13,66 @@ public class DaggeV2 extends TeamRobot {
 
 	private TargetingSystem targetingSystem = new TargetingSystem(this);
 	private MovementSystem movementSystem = new MovementSystem(this);
+	
+	/**
+	 * Specifies basic configurations for DaggeV2 during the game.
+	 * 
+	 * Configures radar and gun movement for more efficient while turning.
+	 */
+	
 	public void run() {
 
-		setAdjustRadarForRobotTurn(true); // Gör radar självständig
-		setAdjustGunForRobotTurn(true); // Gör kanonen självständig
-		// Ska kommenteras bort om boten används med teambot
-//		setBodyColor(new Color(153, 0, 0));
-//		setGunColor(new Color(51, 51, 51));
-//		setRadarColor(new Color(255, 0, 0));
-//		setScanColor(Color.blue);
-//		setBulletColor(Color.red);
-
-		turnRadarRightRadians(Double.POSITIVE_INFINITY); // Scanna kontinuerligt
+		setAdjustRadarForRobotTurn(true); 
+		setAdjustGunForRobotTurn(true); 
+		turnRadarRightRadians(Double.POSITIVE_INFINITY); 
 	}
 
-	@Override
+	/**
+	 * Calls upon the set of actions involved with tracking, targeting, moving and shooting.
+	 * 
+	 * Makes sure DaggeV2 lock onto a target, then moves into optimal distance, 
+	 * then circle strafes around its target while firing continuously with adaptive power.
+	 * Also involves countermeasures for predicted incoming fire.
+	 */
+	
 	public void onScannedRobot(ScannedRobotEvent e) {
-		movementSystem.setData(targetingSystem.getAbsBearing(), targetingSystem.getLatVel()); // Ge
-																								// data
-																								// om
-																								// fienden
-																								// till
-																								// movementSystem
-		targetingSystem.track(e); // Tracka fienden samt sikta
-		movementSystem.move(e); // Utför akuell rörelse
-		targetingSystem.fire(e); // Skjut
-
+		movementSystem.setData(targetingSystem.getAbsBearing(), targetingSystem.getLatVel()); 																	
+		targetingSystem.track(e); 
+		movementSystem.move(e); 
+		targetingSystem.fire(e); 
 	}
-
-	public void onHitWall(HitWallEvent e) { // Undvik väggar
+	
+	/**
+	 * Specifies the actions to be made when collision with a wall occurs.
+	 * Makes sure DaggeV2 turns around and moves away from wall.
+	 */
+	
+	public void onHitWall(HitWallEvent e) { 
 		movementSystem.wallHit(e);
 	}
 
-	public void onHitRobot(HitRobotEvent e) { // Undvik gridlock
+	/**
+	 * Specifies the actions to be made when collision with another robot occurs.
+	 * Makes sure DaggeV2 moves out of melee range.
+	 */
+	
+	public void onHitRobot(HitRobotEvent e) { 
 		movementSystem.collision(e);
 	}
 
-	public void onWin(WinEvent e) { // Dansa vid vinst
+	/**
+	 * Specifies what celebration DaggeV2 does when he wins.
+	 * Makes a neat dance.
+	 */
+	
+	public void onWin(WinEvent e) { 
 		movementSystem.win(e);	
 	}
-
+	
+	/**
+	 * Specifies how DaggeV2 receives colouring specification from a Leader.
+	 */
+	
 	public void onMessageReceived(MessageEvent e) {
 		if (e.getMessage() instanceof RobotColors) {
 			RobotColors c = (RobotColors) e.getMessage();
