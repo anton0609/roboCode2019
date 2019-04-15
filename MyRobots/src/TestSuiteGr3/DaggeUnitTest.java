e5219110ef0344c604e9bb1d52852f85fa16cee7
filip <filip@DESKTOP-FLQPSSC>
2019-04-15 15:15:12 +0200

    Påbörjat sts

---
+++ MyRobots/src/TestSuiteGr3/DaggeUnitTest.java
@@ -0,0 +1,17 @@
+package TestSuiteGr3;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AntiCollision_STS.class})
+class DaggeUnitTest {
+
+	}
+
+}
